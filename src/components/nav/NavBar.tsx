19cf4b4e8a63122d9d546a2af363200e3b6c710a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-03-26 20:40:37 +0000

    Create new state for menuAccount visiblitity and share this state to manuplation of visiblitity.

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import ButtonAccount from "../account/ButtonAccount";
 import MenuAccount from "../account/MenuAccount";
 import Logo from "./Logo";
 import "./NavBar.scss";
 
 export default function NavBar() {
+  const [isAccountMenuVisible, setIsAccountMenuVisible] =
+    useState<boolean>(false);
+
   return (
     <div className="nav-bar">
       <Logo />
-      <ButtonAccount />
-      <MenuAccount />
+      <ButtonAccount setIsAccountMenuVisible={setIsAccountMenuVisible} />
+      <MenuAccount
+        isAccountMenuVisible={isAccountMenuVisible}
+        setIsAccountMenuVisible={setIsAccountMenuVisible}
+      />
     </div>
   );
 }
