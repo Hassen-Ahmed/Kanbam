ce9583a9985973f9bbab4d6e8ece946164e927a7
Hassen-Ahmed <hassenbest23@gmail.com>
2024-03-26 20:44:49 +0000

    Creat interface for ButtonAccount prop and add / import its props and also add onClick event handler to handle menuAccount visibility.

--- src/components/account/ButtonAccount.tsx
+++ src/components/account/ButtonAccount.tsx
@@ -1,9 +1,24 @@
 import "./ButtonAccount.scss";
 
-export default function ButtonAccount() {
+interface IMenuVisiblitySingle {
+  setIsAccountMenuVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ButtonAccount: React.FC<IMenuVisiblitySingle> = ({
+  setIsAccountMenuVisible,
+}) => {
   return (
-    <div className="account-btn">
+    <div
+      className="account-btn"
+      onClick={() => {
+        setIsAccountMenuVisible((preValue) => {
+          return preValue ? false : true;
+        });
+      }}
+    >
       <p>HB</p>
     </div>
   );
-}
+};
+
+export default ButtonAccount;
