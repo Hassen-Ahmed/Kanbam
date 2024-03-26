fdd3abcfff7ad70d4965c4cc45b3ddbf28b849fe
Hassen-Ahmed <hassenbest23@gmail.com>
2024-03-26 23:13:35 +0000

    Import and use Board component.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
+import Board from "../../components/board/Board";
 import NavBar from "../../components/nav/NavBar";
 
 export default function Home() {
   return (
     <div className="home">
       <NavBar />
+      <Board />
     </div>
   );
 }
