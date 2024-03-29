dee6dc4ce08dfb508a87ccb06177a758daecc086
Hassen-Ahmed <hassenbest23@gmail.com>
2024-03-29 22:31:31 +0000

    Create handleTheme which use to swap dark with light theme or viceversal.

--- src/components/account/ButtonTheme.tsx
+++ src/components/account/ButtonTheme.tsx
@@ -4,6 +4,13 @@ export default function ButtonTheme({
 }: {
   setIsAccountMenuVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const handleTheme = (ev: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const id = ev.currentTarget.id;
+
+    id == "theme-light__btn"
+      ? document.body.classList.add("theme-light")
+      : document.body.classList.remove("theme-light");
+  };
   return (
     <div className="menu__theme">
       <h2 className="menu__theme--text">Theme</h2>
@@ -11,10 +18,10 @@ export default function ButtonTheme({
         className="menu__theme--list"
         onClick={() => setIsAccountMenuVisible(false)}
       >
-        <li>
+        <li id="theme-light__btn" onClick={(ev) => handleTheme(ev)}>
           Light <MdLightMode size={18} />
         </li>
-        <li>
+        <li id="theme-dark__btn" onClick={(ev) => handleTheme(ev)}>
           Dark <MdDarkMode size={18} />
         </li>
       </ul>
