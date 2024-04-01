862f62c20f36bd1c7f1ec947b76c1f6103a952eb
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-01 14:49:34 +0100

    Modiy Home comp with its styling.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,15 @@
 import Board from "../../components/board/Board";
 import NavBar from "../../components/nav/NavBar";
-
+import SideBarLeft from "../../components/sidebar/SideBarLeft";
+import "./Home.scss";
 export default function Home() {
   return (
     <div className="home">
       <NavBar />
-      <Board />
+      <div className="home__sub">
+        <SideBarLeft />
+        <Board />
+      </div>
     </div>
   );
 }
