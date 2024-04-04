70f764ea06748c7564504f64d80cbc970932e4cf
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-04 14:29:37 +0100

    Add implementation to Lists comp and modify its styling.

--- src/components/lists/Lists.tsx
+++ src/components/lists/Lists.tsx
@@ -1,6 +1,10 @@
 import Card from "../card/Card";
 import { List, ListsType } from "../../types/lists.type";
+import { BsThreeDots } from "react-icons/bs";
+import { IoMdAdd } from "react-icons/io";
+
 import "./Lists.scss";
+import { useState } from "react";
 
 interface IList {
   list: List;
@@ -10,14 +14,49 @@ interface IList {
 }
 
 const Lists = ({ list, title }: IList) => {
+  const [titleValue, setTitleValue] = useState<string>(title);
+  const [isTitleInputVisible, setIsTitleInputVisible] =
+    useState<boolean>(false);
+
+  const hadlerTitleInputClose = (ev: React.KeyboardEvent<HTMLInputElement>) => {
+    if (ev.key == "Enter") {
+      setIsTitleInputVisible(false);
+    }
+  };
+
   return (
-    <div className="lists--container-main">
-      <div className="lists--container-sub">
-        <h1 className="lists__heading">{title}</h1>
+    <div className="lists--container--main">
+      <div className="lists--container--sub">
+        <h1 className="lists__heading">
+          <div className="lists__heading--text">
+            {!isTitleInputVisible ? (
+              <p onClick={() => setIsTitleInputVisible(true)}>{titleValue}</p>
+            ) : (
+              <input
+                type="text"
+                value={titleValue}
+                onKeyDown={(ev) => hadlerTitleInputClose(ev)}
+                autoFocus
+                spellCheck="false"
+                onBlur={() => setIsTitleInputVisible(false)}
+                onChange={(e) => {
+                  setTitleValue(e.target.value);
+                }}
+              />
+            )}
+          </div>
+          <div className="lists__heading--btn">
+            <BsThreeDots />
+          </div>
+        </h1>
         <div className="lists">
           {list.map((content) => {
             return <Card {...content} />;
           })}
+          <div className="lists__btn--add">
+            <IoMdAdd size={20} />
+            <p>Add a Card</p>
+          </div>
         </div>
       </div>
     </div>
