79f5ec3c7d3933dd13ff291b63bfc1c97c6ec548
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-13 22:40:22 +0100

    import types, useRef and also modify kanbam constant variable with board and also its contents also some attributes and scss styling file.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,72 +1,80 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Lists from "../lists/Lists";
-import { ListsType } from "../../types/lists.type";
-import { IKanbam } from "../../types/kanbam.type";
+import { ICard } from "../../types/lists.type";
+import { BoardType } from "../../types/board.type";
 import { IoMdAdd } from "react-icons/io";
 import "./Board.scss";
 
-const kanbam: IKanbam = {
-  theme: "light",
-  lists: [
-    {
-      id: 1,
-      title: "title",
-      list: [
-        {
-          id: 1,
-          title: "Heading 8",
-          description: "Lorem, Pariatur sunt esse modi ",
-          isDragging: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "title",
-      list: [
-        {
-          id: 2,
-          title: "Heading 8",
-          description:
-            "Lorem, Pariatur sunt esse modi earum rem blanditiis Lorem, s",
-          isDragging: false,
-        },
-        {
-          id: 3,
-          title: "Heading 8",
-          description:
-            "Lorem, Pariatur sunt esse modi earum rem blanditiis Lorem, ",
-          isDragging: false,
-        },
-        {
-          id: 4,
-          title: "Heading 8",
-          description:
-            "Lorem, Pariatur sunt esse modi earum rem blanditiis Lorem, ",
-          isDragging: false,
-        },
-      ],
-    },
-    // {
-    //   id: 3,
-    //   title: "title",
-    //   list: [],
-    // },
-    // {
-    //   id: 4,
-    //   title: "title",
-    //   list: [],
-    // },
-    // {
-    //   id: 5,
-    //   title: "title",
-    //   list: [],
-    // },
-  ],
-};
+const board: BoardType = [
+  // id: objectId / id
+  {
+    id: 1,
+    title: "title",
+    isDragging: false,
+    indexNumber: 0,
+    list: [
+      {
+        id: 1,
+        title: "Heading 8",
+        description: "Lorem, Pariatur sunt esse modi ",
+        isDragging: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "title",
+    isDragging: false,
+    indexNumber: 1,
+    list: [
+      {
+        id: 2,
+        title: "Heading Lorem, Pariatur sunt Lorem, Pariatur sunt",
+        description: "Lorem, Pariatur sunt",
+        isDragging: false,
+      },
+      {
+        id: 3,
+        title: "Heading 8",
+        description:
+          "Lorem, Pariatur sunt esse modi earum rem blanditiis Lorem, ",
+        isDragging: false,
+      },
+      {
+        id: 4,
+        title: "Heading 8",
+        description:
+          "Lorem, Pariatur sunt esse modi earum rem blanditiis Lorem, ",
+        isDragging: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "title",
+    isDragging: false,
+    indexNumber: 2,
+    list: [],
+  },
+  {
+    id: 4,
+    title: "title",
+    isDragging: false,
+    indexNumber: 3,
+    list: [],
+  },
+  {
+    id: 5,
+    title: "title",
+    isDragging: false,
+    indexNumber: 4,
+    list: [],
+  },
+];
 
 const Board = () => {
-  const [lists, setLists] = useState<ListsType>(kanbam.lists);
+  const [lists, setLists] = useState<BoardType>(board);
+  const itemDragging = useRef<ICard | null>(null);
 
   return (
     <div className="board-container">
@@ -76,10 +84,12 @@ const Board = () => {
           return (
             <Lists
               key={list.id}
+              id={list.id}
               list={list.list}
               setLists={setLists}
               idOfList={list.id}
               title={list.title}
+              itemDragging={itemDragging}
             />
           );
         })}
