b9efd5245ec97e018f99fc353faab72a7832d597
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-13 22:35:51 +0100

    create new attributes for dragging and functions to handle dragStart and modify its scss styling file.

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,15 +1,42 @@
 import { ICard } from "../../types/lists.type";
+import { DragEventMy } from "../../types/html.type";
 import "./Card.scss";
+import { handleDragstartUtil } from "../../utils/dnd";
 
-const Card = ({ title }: ICard) => {
+interface ICardExtended extends ICard {
+  itemDragging: React.MutableRefObject<ICard | null>;
+}
+
+const Card = ({
+  id,
+  title,
+  description,
+  isDragging,
+  itemDragging,
+}: ICardExtended) => {
+  const handleDragStart = (ev: DragEventMy) => {
+    itemDragging.current = {
+      id,
+      title,
+      description,
+      isDragging,
+    };
+
+    if (!(ev.target instanceof HTMLDivElement)) return;
+    handleDragstartUtil(ev, "card");
+  };
+
   return (
-    <div className="card-container">
+    <div
+      className="card-container"
+      draggable="true"
+      onDragStart={(ev) => handleDragStart(ev)}
+      data-id={id}
+      data-identity="card"
+    >
       <div className="card">
         <div className="card__heading">
-          <h2>
-            {title} someting else Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Atque, quos.
-          </h2>
+          <h2>{title} someting else.</h2>
         </div>
       </div>
     </div>
