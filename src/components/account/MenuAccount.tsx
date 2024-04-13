7690aa3847b07c3bd8d40cb26dd15256c84572f8
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-13 22:38:34 +0100

    Add new elements, fix visibility handling and its scss styling file.

--- src/components/account/MenuAccount.tsx
+++ src/components/account/MenuAccount.tsx
@@ -12,34 +12,39 @@ const MenuAccount = ({
   setIsAccountMenuVisible,
 }: IMenuVisiblity) => {
   return (
-    <div
-      className="menu-account"
-      style={{
-        zIndex: isAccountMenuVisible ? 2000 : 0,
-      }}
-    >
-      {isAccountMenuVisible && (
-        <>
-          <div className="menu">
-            <h2 className="menu__heading">Account</h2>
+    <>
+      {!isAccountMenuVisible ? null : (
+        <div
+          className="menu-account"
+          style={{
+            zIndex: isAccountMenuVisible ? 2000 : 0,
+          }}
+        >
+          {isAccountMenuVisible && (
+            <>
+              <div className="menu">
+                <h2 className="menu__heading">Account</h2>
+                <MenuAccountLogo />
+                <hr />
+                <ButtonTheme />
+                <hr />
+                <div className="menu__logout">
+                  <h2 className="menu__logout--text">Logout</h2>
+                </div>
+                <div className="menu__donate">
+                  <h2 className="menu__donate--text">Donate</h2>
+                </div>
+              </div>
 
-            <MenuAccountLogo />
-            <hr />
-            <ButtonTheme />
-            <hr />
-
-            <div className="menu__logout">
-              <h2 className="menu__logout--text">Logout</h2>
-            </div>
-          </div>
-
-          <div
-            className="menu__overlay"
-            onClick={() => setIsAccountMenuVisible(false)}
-          ></div>
-        </>
+              <div
+                className="menu__overlay"
+                onClick={() => setIsAccountMenuVisible(false)}
+              ></div>
+            </>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
