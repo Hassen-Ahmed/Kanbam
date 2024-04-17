f2c5216763729dab0fb5ce1c135919c4fcd546c5
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-17 14:15:59 +0100

    Create new Loading notification comp and its styling.

---
+++ src/components/notifications/Loading.tsx
@@ -0,0 +1,9 @@
+import "./Loading.scss";
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading__icon"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
