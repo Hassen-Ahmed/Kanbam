54f815e1d6ee041abbb7321cc4da320aa7b4563c
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-17 14:22:42 +0100

    Create boardReducer for handling GET_ALL_LISTS and ADD_LIST  functionality.

---
+++ src/reducers/boardReducer.ts
@@ -0,0 +1,15 @@
+import { IActionBoard } from "../types/actions.type";
+import { BoardType } from "../types/board.type";
+
+export const boardReducer = (state: BoardType, action: IActionBoard) => {
+  switch (action.type) {
+    case "GET_ALL_LISTS":
+      return action.payload;
+
+    case "ADD_LIST":
+      return [...state, ...action.payload];
+
+    default:
+      return state;
+  }
+};
