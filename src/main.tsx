321179f74440d5f6291f4b920662da2afbf3b5fc
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-17 14:24:28 +0100

    Wrap only App comp by kanbamContextProvider.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/style/main.scss";
+import KanbamContextProvider from "./context/kanbamContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <KanbamContextProvider>
+      <App />
+    </KanbamContextProvider>
   </React.StrictMode>
 );
