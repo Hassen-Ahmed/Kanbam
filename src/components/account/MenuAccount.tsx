ef62b0f592c7bbbc1b4c8f1098c04e7f18718864
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-17 23:47:21 +0100

    Create Confetti comp to MenuAcount comp and modify its styling.

--- src/components/account/MenuAccount.tsx
+++ src/components/account/MenuAccount.tsx
@@ -1,6 +1,9 @@
+import { createPortal } from "react-dom";
 import ButtonTheme from "./ButtonTheme";
 import "./MenuAccount.scss";
 import MenuAccountLogo from "./MenuAccountLogo";
+import { useEffect, useRef, useState } from "react";
+import Confetti from "react-confetti";
 
 interface IMenuVisiblity {
   isAccountMenuVisible: boolean;
@@ -11,13 +14,39 @@ const MenuAccount = ({
   isAccountMenuVisible,
   setIsAccountMenuVisible,
 }: IMenuVisiblity) => {
+  const [areWeCelebrating, setAreWeCelebrating] = useState(false);
+
+  const dimension = useRef<{ width: number; height: number }>();
+
+  const confetti = (
+    <div>
+      <Confetti
+        width={dimension.current?.width}
+        height={dimension.current?.height}
+        recycle={false}
+        tweenDuration={5000}
+        initialVelocityY={2}
+        style={{ zIndex: 2100 }}
+        gravity={0.2}
+        wind={0.05}
+        numberOfPieces={1000}
+      />
+    </div>
+  );
+
+  useEffect(() => {
+    dimension.current = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }, []);
   return (
     <>
       {!isAccountMenuVisible ? null : (
         <div
           className="menu-account"
           style={{
-            zIndex: isAccountMenuVisible ? 2000 : 0,
+            zIndex: isAccountMenuVisible ? 2100 : 0,
           }}
         >
           {isAccountMenuVisible && (
@@ -31,7 +60,17 @@ const MenuAccount = ({
                 <div className="menu__logout">
                   <h2 className="menu__logout--text">Logout</h2>
                 </div>
-                <div className="menu__donate">
+
+                {areWeCelebrating && createPortal(confetti, document.body)}
+
+                <div
+                  className="menu__donate"
+                  onClick={() => {
+                    setAreWeCelebrating((preValue) => {
+                      return preValue ? false : true;
+                    });
+                  }}
+                >
                   <h2 className="menu__donate--text">Donate</h2>
                 </div>
               </div>
