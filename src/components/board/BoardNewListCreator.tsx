f49c6095df061a6d2e04550d3a461ea6e0f4754a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-17 14:13:12 +0100

    Create new BoardNewListCreator comp and its styling which handle creating new list.

---
+++ src/components/board/BoardNewListCreator.tsx
@@ -0,0 +1,62 @@
+import { MdOutlineCancel } from "react-icons/md";
+import "./BoardNewListCreator.scss";
+import { useContext, useState } from "react";
+import { IkanbamContext, KanbamContext } from "../../context/kanbamContext";
+// import { BoardType } from "../../types/board.type";
+import { IList } from "../../types/lists.type";
+import { postList } from "../../utils/api/posts";
+
+type isListAddedType = { isListAddedSetter: (value: boolean) => void };
+
+const BoardNewListCreator = ({ isListAddedSetter }: isListAddedType) => {
+  const [inputList, setInputList] = useState<string>("");
+  const { lists, dispatch } = useContext(KanbamContext) as IkanbamContext;
+
+  const handleAddList = async () => {
+    if (inputList.length) {
+      let newList: IList = {
+        title: inputList,
+        indexNumber: lists?.length,
+      };
+      try {
+        const data = await postList(newList);
+
+        newList = { ...data, list: [] };
+        console.log(newList);
+
+        dispatch({ type: "ADD_LIST", payload: [newList] });
+        setInputList("");
+      } catch (err) {
+        console.log(`Error message: ${err}`);
+      } finally {
+        console.log("Send POST request for new list...");
+      }
+    }
+  };
+
+  return (
+    <div className="board__new-list board__new-list--creator">
+      <div className="board__new-list--input">
+        <input
+          type="text"
+          autoFocus
+          value={inputList}
+          onChange={(ev) => setInputList(ev.target.value)}
+        />
+      </div>
+      <div className="board__new-list--btns">
+        <div className="board__new-list--btns-add" onClick={handleAddList}>
+          <button>Add list</button>
+        </div>
+        <div
+          className="board__new-list--btns-cancel"
+          onClick={() => isListAddedSetter(false)}
+        >
+          <MdOutlineCancel size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardNewListCreator;
