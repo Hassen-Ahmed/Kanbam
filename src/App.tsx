9d412cceca29bfac458faf1590c9b9fd1098b8a8
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-17 14:18:56 +0100

    Create new functionality which handle the getAllLists http request and dispatch the payload to global lists.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { useContext, useEffect } from "react";
+import { IkanbamContext, KanbamContext } from "./context/kanbamContext";
 import Home from "./pages/home/Home";
+import { getAllLists } from "./utils/api/gets";
 
 const App = () => {
+  const { dispatch } = useContext(KanbamContext) as IkanbamContext;
+
+  const fetchAllLists = async () => {
+    try {
+      const res = await getAllLists();
+      console.log(res);
+      dispatch({ type: "GET_ALL_LISTS", payload: res });
+    } catch (err) {
+      console.log(`Error message: ${err}`);
+    } finally {
+      console.log("Alreay sent the request.");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllLists();
+  }, []);
+
   return (
     <div className="app">
       <Home />
