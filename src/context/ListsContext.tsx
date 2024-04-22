ed8f2a5f39af89464bd504512b56ef246bd1ae33
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-22 16:48:43 +0100

    Create new ListsContext and ListsContextProvider to handle lists and dispatch.

---
+++ src/context/ListsContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useReducer } from "react";
+import { BoardType } from "../types/board.type";
+import { boardReducer } from "../reducers/boardReducer";
+import { IActionBoard } from "../types/actions.type";
+
+export interface IListsContext {
+  lists: BoardType | null;
+  dispatch: React.Dispatch<IActionBoard>;
+}
+
+export const ListsContext = createContext<IListsContext | null>(null);
+
+const ListsContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [lists, dispatch] = useReducer(boardReducer, []);
+
+  return (
+    <ListsContext.Provider value={{ lists, dispatch }}>
+      {children}
+    </ListsContext.Provider>
+  );
+};
+
+export default ListsContextProvider;
