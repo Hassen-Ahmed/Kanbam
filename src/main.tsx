587a002314c4a73bb88c19f0da56d43e48d7a53d
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-22 16:49:27 +0100

    Wrap KanbamContextProvider by ListsContextProvider context.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/style/main.scss";
 import KanbamContextProvider from "./context/kanbamContext.tsx";
+import ListsContextProvider from "./context/ListsContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <KanbamContextProvider>
-      <App />
-    </KanbamContextProvider>
+    <ListsContextProvider>
+      <KanbamContextProvider>
+        <App />
+      </KanbamContextProvider>
+    </ListsContextProvider>
   </React.StrictMode>
 );
