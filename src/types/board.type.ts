71df6cbe449b81e0b1cfbb1c05a6bbf6a9fe2d0a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-27 13:22:37 +0100

    Add more types those are drived from removed Lists.type.ts .

--- src/types/board.type.ts
+++ src/types/board.type.ts
@@ -1,3 +1,21 @@
-import { IList } from "./lists.type";
+export interface ICard {
+  id?: string;
+  listId: string;
+  title: string;
+  indexNumber: number;
+  isDragging?: boolean;
+  opacity?: string;
+}
+
+export type ListType = ICard[];
+
+export interface IList {
+  id?: string;
+  title: string;
+  isDragging?: boolean;
+  indexNumber: number;
+  list?: ListType;
+  opacity?: string;
+}
 
 export type BoardType = IList[];
