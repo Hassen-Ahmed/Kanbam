c129f0fb8228b3a98d3a27bc26b84e1315fd7e5a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-04-28 21:39:09 +0100

    Add null type that union with BoardType for state and modify other.

--- src/reducers/boardReducer.ts
+++ src/reducers/boardReducer.ts
@@ -1,13 +1,13 @@
 import { IActionBoard } from "../types/actions.type";
 import { BoardType } from "../types/board.type";
 
-export const boardReducer = (state: BoardType, action: IActionBoard) => {
+export const boardReducer = (state: BoardType | null, action: IActionBoard) => {
   switch (action.type) {
     case "GET_ALL_LISTS":
       return action.payload;
 
     case "ADD_LIST":
-      return [...state, ...action.payload];
+      return [...state!, ...action.payload];
 
     default:
       return state;
