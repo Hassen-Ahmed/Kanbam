daac0c5e54edd43e83adb73acbb2be23d516bf70
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-05 15:29:56 +0100

    Modify the look of Home comp, add icons with link anchor, and also modify its structure and styling.

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -2,8 +2,11 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Home.scss";
 import { useEffect, useState } from "react";
 import { isTokenAuthenticated } from "../../utils/jwtAuth";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 import Loading from "../../components/notifications/Loading";
+
+const iconsSize = 30;
 
 export default function Home() {
   const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
@@ -26,18 +29,35 @@ export default function Home() {
     );
 
   return (
-    <div className="home">
-      <h1>⭐️ Welcome to Kanbam! ⭐️</h1>
-      <p>
-        Kanbam is a web application inspired by Trello's kanban board, designed
-        to help teams manage projects and tasks effectively.
-      </p>
-
-      <div className="home__btn">
-        <Link to="/auth">
-          <button>Get started!</button>
-        </Link>
+    <div className="home-container">
+      <div className="home">
+        <main className="home__main">
+          <h2>From Hassen</h2>
+          <h1>Welcome to Kanbam!</h1>
+          <div className="home__main--btn">
+            <Link to="/auth">
+              <button>Get started!</button>
+            </Link>
+          </div>
+        </main>
+        <div className="home__links">
+          <a href="https://github.com/Hassen-Ahmed/Kanbam" target="_blank">
+            <FaGithub size={iconsSize} />
+          </a>
+          <a href="https://www.linkedin.com/in/hassen-abdela/" target="_blank">
+            <FaLinkedin size={iconsSize} />
+          </a>
+        </div>
       </div>
+      <footer className="home__footer">
+        <h2>What is Kanbam:?</h2>
+
+        <p>
+          Kanbam is a web application inspired by Trello's kanban board,
+          designed to help teams manage projects and tasks effectively.
+        </p>
+        <span>&copy;Copywrite 2024. By Hassen. </span>
+      </footer>
     </div>
   );
 }
