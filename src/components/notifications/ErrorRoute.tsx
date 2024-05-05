e01d0a9390474fbbb6b1d68f8494b7e05798dad0
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-05 15:22:34 +0100

    Modify ErrorRoute comp structure and its styling.

--- src/components/notifications/ErrorRoute.tsx
+++ src/components/notifications/ErrorRoute.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useRouteError } from "react-router-dom";
-
-export default function ErrorPage({ text }: { text: string }) {
+import "./ErrorPage.scss";
+export default function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
     <div className="error-route">
-      <h1>Oops! </h1>
-      <p>Sorry, an unexpected error has occurred on {text}.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <div className="contents">
+        <h1>Oops! </h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>404 {error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
   );
 }
