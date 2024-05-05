9550d5d35907bf637224e585bb54e39beebbb9fb
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-05 15:31:41 +0100

    Add navigation that navigae to kanbam/board based on if token is authenticate, and also include Loading comp for notification.

--- src/routes/auth/Auth.tsx
+++ src/routes/auth/Auth.tsx
@@ -1,7 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./Auth.scss";
+import { useEffect, useState } from "react";
+import { isTokenAuthenticated } from "../../utils/jwtAuth";
+import Loading from "../../components/notifications/Loading";
 
 export default function Auth() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isTokenAuthenticated()) {
+      navigate("/kanbam/board");
+      setIsUserAuthenticated(true);
+    } else {
+      setIsUserAuthenticated(false);
+    }
+  }, []);
+
+  if (isUserAuthenticated)
+    return (
+      <>
+        <Loading />;
+      </>
+    );
+
   return (
     <div className="auth">
       <h2>⭐️ Welcome again! ⭐️</h2>
