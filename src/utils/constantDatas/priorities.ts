bd6f7322844092e9873eb0815b4f84ddeeea1ff7
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-06 23:01:14 +0100

    Create priorities util ts file that hold data of different priority properities like name, color, and diretion.

---
+++ src/utils/constantDatas/priorities.ts
@@ -0,0 +1,7 @@
+export const priorities = [
+  { name: "Highest", color: "#ec2a2aa2", direction: "0deg" },
+  { name: "High", color: "#da4e4ea2", direction: "0deg" },
+  { name: "Medium", color: "#da7d4ea2", direction: "180deg" },
+  { name: "Low", color: "#22ce45a2", direction: "180deg" },
+  { name: "Lowest", color: "#4eda6aa2", direction: "180deg" },
+];
