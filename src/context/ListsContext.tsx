f4e07b9d82c9ea1dc4530a26028f31f6ecd7363c
Hassen-Ahmed <hassenbest23@gmail.com>
2024-05-07 14:17:44 +0100

    Create searchText, setSearchText and handleSearchTextUpdate function to handle updating of searchText.

--- src/context/ListsContext.tsx
+++ src/context/ListsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState } from "react";
 import { BoardType } from "../types/board.type";
 import { boardReducer } from "../reducers/boardReducer";
 import { IActionBoard } from "../types/actions.type";
@@ -6,15 +6,24 @@ import { IActionBoard } from "../types/actions.type";
 export interface IListsContext {
   lists: BoardType | null;
   dispatch: React.Dispatch<IActionBoard>;
+  searchText: string;
+  handleSearchTextUpdate: (text: string) => void;
 }
 
 export const ListsContext = createContext<IListsContext | null>(null);
 
 const ListsContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [lists, dispatch] = useReducer(boardReducer, null);
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchTextUpdate = (text: string) => {
+    setSearchText(text.toLocaleLowerCase());
+  };
 
   return (
-    <ListsContext.Provider value={{ lists, dispatch }}>
+    <ListsContext.Provider
+      value={{ lists, dispatch, searchText, handleSearchTextUpdate }}
+    >
       {children}
     </ListsContext.Provider>
   );
