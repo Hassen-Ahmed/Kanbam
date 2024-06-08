3d50aaa7ffb65086b90208d8218a340c2e1c6823
Hassen-Ahmed <hassenbest23@gmail.com>
2024-06-08 14:10:25 +0100

    Changed name of direction to rotation.

--- src/utils/constantDatas/priorities.ts
+++ src/utils/constantDatas/priorities.ts
@@ -1,7 +1,7 @@
 export const priorities = [
-  { name: "Highest", color: "#ec2a2aa2", direction: "0deg" },
-  { name: "High", color: "#da4e4ea2", direction: "0deg" },
-  { name: "Medium", color: "#da7d4ea2", direction: "180deg" },
-  { name: "Low", color: "#22ce45a2", direction: "180deg" },
-  { name: "Lowest", color: "#4eda6aa2", direction: "180deg" },
+  { name: "Highest", color: "#ec2a2aa2", rotation: "0deg" },
+  { name: "High", color: "#da4e4ea2", rotation: "0deg" },
+  { name: "Medium", color: "#da7d4ea2", rotation: "180deg" },
+  { name: "Low", color: "#22ce45a2", rotation: "180deg" },
+  { name: "Lowest", color: "#4eda6aa2", rotation: "180deg" },
 ];
