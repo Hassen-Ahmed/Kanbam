3f04616fd11a8741fda09fec140bab36b722ee47
Hassen-Ahmed <hassenbest23@gmail.com>
2024-06-13 22:50:01 +0100

    Modified ButtonAccount.test

--- src/__test__/__unit__/components/account/ButtonAccount.test.tsx
+++ src/__test__/__unit__/components/account/ButtonAccount.test.tsx
@@ -2,9 +2,12 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import ButtonAccount from "../../../../components/account/ButtonAccount";
 import { userEvent } from "@testing-library/user-event";
 
+const sumFunc = (a: number, b: number) => a + b;
+const mockFunc = vi.fn().mockImplementation(sumFunc);
+
 describe("ButtonAccount", () => {
-  test("It will have element with text HB", () => {
-    const mockFunc = vi.fn();
+  test("It has element with text HB", () => {
+    //
     render(<ButtonAccount setIsAccountMenuVisible={mockFunc} />);
 
     const paraElement = screen.getByText(/hb/i);
@@ -12,14 +15,15 @@ describe("ButtonAccount", () => {
     expect(paraElement.innerHTML).toBe("HB");
     expect(paraElement).toBeInTheDocument();
   });
+  //
   test("It will NOT call props function when there is no click event happend", () => {
-    const mockFunc = vi.fn();
+    //
     render(<ButtonAccount setIsAccountMenuVisible={mockFunc} />);
-
     expect(mockFunc).toBeCalledTimes(0);
   });
-  test("It will call props function when it CLICKed", async () => {
-    const mockFunc = vi.fn();
+  //
+  test("It will call props function and return correct result when it CLICKed", async () => {
+    //
     render(<ButtonAccount setIsAccountMenuVisible={mockFunc} />);
 
     const accontBtn = document.getElementsByClassName("account-btn")[0];
@@ -35,5 +39,6 @@ describe("ButtonAccount", () => {
     );
 
     expect(mockFunc).toBeCalledTimes(2);
+    expect(mockFunc(2, 2)).toBe(4);
   });
 });
