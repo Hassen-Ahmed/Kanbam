2c68f3903df4378059e1c73218a46bca2a819a8e
Hassen-Ahmed <hassenbest23@gmail.com>
2024-06-16 19:05:38 +0100

    Modified setup.ts file by creating localStorage property to window object

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,6 +5,18 @@ import { afterEach } from "vitest";
 beforeEach(() => {
   // will clear mock history
   vi.clearAllMocks();
+
+  // manual mocking of localStorage
+  // will create localStorage property to window object
+  Object.defineProperty(window, "localStorage", {
+    value: {
+      getItem: vi.fn(),
+      setItem: vi.fn(),
+      removeItem: vi.fn(),
+      clear: vi.fn(),
+    },
+    writable: true,
+  });
 });
 
 afterEach(() => {
