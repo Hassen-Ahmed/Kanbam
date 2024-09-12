fb8732264c1bb94cbf0c257ec9709906d1bf4d00
Hassen-Ahmed <hassenbest23@gmail.com>
2024-09-12 14:30:52 +0100

    feat(front-end): created new Event.tsx component, which define events inside caledar and define edit button to manuiplate tasks

---
+++ src/components/calendar/Event.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MdEditNote } from "react-icons/md";
+import { ICard } from "../../types/board.type";
+
+interface IEvent {
+  eventInfo: any;
+  cardDetails: ICard[] | null;
+  setCardDetail: React.Dispatch<React.SetStateAction<ICard | null>>;
+  setShowModalCard: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function Event({
+  eventInfo,
+  cardDetails,
+  setCardDetail,
+  setShowModalCard,
+}: IEvent) {
+  return (
+    <div className="event">
+      <span className="event__title">{eventInfo.event.title} </span>
+      <div className="event-calendar-edite--btn-container">
+        <div
+          className="event-calendar-edite--btn"
+          onClick={() => {
+            setCardDetail(
+              () =>
+                cardDetails?.filter(
+                  (card) => card.id == eventInfo.event.id
+                )[0] as ICard
+            );
+            setShowModalCard(true);
+          }}
+        >
+          <MdEditNote size={18} />
+        </div>
+      </div>
+    </div>
+  );
+}
