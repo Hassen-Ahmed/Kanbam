c814c31427058e400aa071a8e644034cb304ac4a
Hassen-Ahmed <hassenbest23@gmail.com>
2024-09-15 18:41:35 +0100

    fix(front-end): removed asigning background color based on location pathname

--- src/routes/kanbam/Kanbam.tsx
+++ src/routes/kanbam/Kanbam.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "../../components/nav/NavBar";
 import SideBarLeft from "../../components/sidebar/SideBarLeft";
 import { ListsContext } from "../../context/ListsContext";
@@ -11,29 +11,20 @@ import { INewTheme } from "../../types/styledComp";
 import { IkanbamContext, KanbamContext } from "../../context/kanbamContext";
 import { themes } from "../../utils/constantDatas/themes";
 
-interface INewTheme2 extends INewTheme {
-  $url: string;
-}
-
-export const GlobalStyle = styled.div<INewTheme2>`
+export const GlobalStyle = styled.div<INewTheme>`
   ::-webkit-scrollbar-thumb {
     border-radius: 1rem;
     background-color: ${({ $newtheme }) => themes[$newtheme].bg["scroll_01"]};
   }
-
-  background-color: ${({ $newtheme, $url }) =>
-    $url.includes("board") ? "none" : themes[$newtheme].bg["card_modal"]};
 `;
 
 export default function Kanbam() {
   const { lists } = useContext(ListsContext) as IListsContext;
   const { theme } = useContext(KanbamContext) as IkanbamContext;
-  const location = useLocation();
 
   return (
     <GlobalStyle
       $newtheme={theme}
-      $url={location.pathname}
       className="kanbam"
       onDrop={() => handleAppOnDrop(lists)}
     >
