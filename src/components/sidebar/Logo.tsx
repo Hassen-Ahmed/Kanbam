f71ed3be224aba8d21b9c03cb565fcda5cdb61e4
Hassen-Ahmed <hassenbest23@gmail.com>
2024-09-26 13:43:11 +0100

    feat(front-end): created new Logo.tsx component and its Logo.scss file inside sidebar component dir

---
+++ src/components/sidebar/Logo.tsx
@@ -0,0 +1,12 @@
+import "./Logo.scss";
+
+const Logo = () => {
+  return (
+    <div className="logo-kanbam">
+      <div className="logo__icon"></div>
+      <h1 className="logo__heading">Kanbam</h1>
+    </div>
+  );
+};
+
+export default Logo;
