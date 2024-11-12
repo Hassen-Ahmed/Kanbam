1c0487390d023ff9c2e345ffc670d429b6e9f4fd
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-12 10:43:03 +0000

    fix(front-end): modified hard-coded value to be dynamic

--- src/components/account/MenuAccountLogo.tsx
+++ src/components/account/MenuAccountLogo.tsx
@@ -1,17 +1,20 @@
+import { useContext } from "react";
 import { ImCamera } from "react-icons/im";
+import { IkanbamContext, KanbamContext } from "../../context/kanbamContext";
 
 export default function MenuAccountLogo() {
+  const { userDetail } = useContext(KanbamContext) as IkanbamContext;
   return (
     <div className="menu__logo">
       <div className="menu__logo--icon">
-        <p>HB</p>
+        <p>{userDetail?.userName.slice(0, 2).toUpperCase()}</p>
       </div>
       <div className="menu__logo--photo">
         <ImCamera size={16} />
       </div>
       <div className="menu__logo--texts">
-        <h2 className="menu__logo--user">Test Account</h2>
-        <h3 className="menu__logo--email">test@gmail.com</h3>
+        <h2 className="menu__logo--user">{userDetail?.userName}</h2>
+        <h3 className="menu__logo--email">{userDetail?.email}</h3>
       </div>
     </div>
   );
