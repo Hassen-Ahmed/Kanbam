d890a239e58f0211d2f441dcffdaab89b9651dd4
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-12 11:21:44 +0000

    feat(front-end): created WorkspaceList component and its stylng file to handle or dispaly list of workspaces

---
+++ src/components/workspace/WorkspaceList.tsx
@@ -0,0 +1,203 @@
+import useFetchAllWorkspace from "../../hooks/useFetchAllWorkspace";
+import "./Workspaces.scss";
+import { INewTheme } from "../../types/styledComp";
+import styled from "styled-components";
+import { useContext, useState } from "react";
+import { IkanbamContext, KanbamContext } from "../../context/kanbamContext";
+import { themes } from "../../utils/constantDatas/themes";
+import { IoMdAdd } from "react-icons/io";
+import NewItem, { IItemDetail } from "./components/NewItem";
+import Loading from "../notifications/Loading";
+import { postWorkspace } from "../../utils/api/posts";
+import { IError } from "../../types/status.type";
+import Item from "./components/Item";
+import { deleteWorkspaceById } from "../../utils/api/deletes";
+import AreYouSure from "../../utils/areYouSure/AreYouSure";
+import UpdateItem from "./components/UpdateItem";
+import { updateWorkspace } from "../../utils/api/updates";
+import ErrorMessage from "../notifications/ErrorMessage";
+
+const WorkspacesStyled = styled.div<INewTheme>`
+  background-color: ${({ $newtheme }) => themes[$newtheme].bg["lists"]};
+  color: ${({ $newtheme }) => themes[$newtheme].font["secondary"]};
+
+  .workspaces__top {
+    background-color: ${({ $newtheme }) => themes[$newtheme].bg["card"]};
+  }
+  .item {
+    border: 0.2rem solid
+      ${({ $newtheme }) => themes[$newtheme].border["secondary"]};
+  }
+`;
+
+export default function WorkspaceList() {
+  const { theme } = useContext(KanbamContext) as IkanbamContext;
+  const { data, loading, error, refetch } = useFetchAllWorkspace();
+
+  const [showNewItemModal, setShowNewItemModal] = useState(false);
+  const [showUpdateItemModal, setShowUpdateItemModal] = useState(false);
+
+  const [requestError, setRequestError] = useState(false);
+  const [showAreYouSureModal, setShowAreYouSureModal] = useState(false);
+  const [IdToModify, setIdToModify] = useState("");
+
+  const handleNewItemModlaVisibility = (value: boolean) => {
+    setShowNewItemModal(value);
+  };
+
+  const handleUpdateItemModlaVisibility = (value: boolean) => {
+    setShowUpdateItemModal(value);
+  };
+
+  const handleItemCreation = async (item: IItemDetail) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        throw new Error("No token found");
+      }
+
+      await postWorkspace(item, token);
+      handleNewItemModlaVisibility(false);
+      refetch();
+    } catch (err) {
+      setRequestError(true);
+      const error = err as IError;
+      console.log("Error Creating Board: ", error.message);
+    }
+  };
+
+  const handleItemUpdate = async (item: IItemDetail) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        throw new Error("No token found");
+      }
+
+      const modifiedItem = { workspaceId: IdToModify!, ...item };
+      await updateWorkspace(IdToModify, modifiedItem, token);
+      handleUpdateItemModlaVisibility(false);
+      refetch();
+    } catch (err) {
+      setRequestError(true);
+      const error = err as IError;
+      console.log("Error Creating Board: ", error.message);
+    }
+  };
+
+  const handleAreYouSure = (status: boolean) => {
+    if (!status) {
+      setShowAreYouSureModal(false);
+    } else {
+      handleItemDeletion();
+    }
+  };
+
+  const clickToDelete = (status: boolean, id: string) => {
+    setShowAreYouSureModal(status);
+    setIdToModify(id);
+  };
+
+  const handleItemDeletion = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        throw new Error("No token found");
+      }
+      await deleteWorkspaceById(IdToModify, token);
+      refetch();
+    } catch (err) {
+      setRequestError(true);
+      const error = err as IError;
+      console.log("Error Creating Board: ", error.message);
+    } finally {
+      setShowAreYouSureModal(false);
+    }
+  };
+
+  //
+
+  if (loading)
+    return (
+      <div className="loading-notification__container">
+        <Loading />
+      </div>
+    );
+
+  if (error) return <ErrorMessage />;
+
+  return (
+    <div className="workspaces-contianer">
+      <WorkspacesStyled $newtheme={theme} className="workspaces">
+        <div className="workspaces__top">
+          <div className="user-detail">
+            <h3>{data?.userDetail.userName}</h3>
+            <h4>{data?.userDetail.email}</h4>
+          </div>
+          <div
+            className="create-workspace"
+            onClick={() => {
+              handleNewItemModlaVisibility(true);
+              setRequestError(false);
+            }}
+          >
+            <button>Create Workspace</button>
+            <IoMdAdd size={25} />
+          </div>
+        </div>
+        <div className="workspaces__bottom">
+          <div className="heading">
+            <h1>Workspaces</h1>
+          </div>
+          <div className="list">
+            {data?.workspaces.length == 0 ? (
+              <p className="empty-data">No items available to dispaly.</p>
+            ) : (
+              data?.workspaces.map((w) => {
+                return (
+                  <Item
+                    key={w.workspaceId}
+                    type={"w"}
+                    id={w.workspaceId}
+                    name={w.name}
+                    description={w.description}
+                    boardAccessLevel={w.boardAccessLevel}
+                    clickToDelete={clickToDelete}
+                    handleUpdateItemModlaVisibility={
+                      handleUpdateItemModlaVisibility
+                    }
+                    setRequestError={setRequestError}
+                    setIdToModify={setIdToModify}
+                  />
+                );
+              })
+            )}
+          </div>
+        </div>
+      </WorkspacesStyled>
+      {showAreYouSureModal && (
+        <AreYouSure handleAreYouSure={handleAreYouSure} />
+      )}
+
+      {showNewItemModal && (
+        <NewItem
+          type={"Workspace"}
+          requestError={requestError}
+          handleItemCreation={handleItemCreation}
+          handleNewItemModlaVisibility={handleNewItemModlaVisibility}
+        />
+      )}
+
+      {showUpdateItemModal && (
+        <UpdateItem
+          type={"Workspace"}
+          requestError={requestError}
+          handleItemUpdate={handleItemUpdate}
+          handleUpdateItemModlaVisibility={handleUpdateItemModlaVisibility}
+        />
+      )}
+    </div>
+  );
+}
