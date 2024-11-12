eb25a3b060301d11ed19124572956af8f594a30d
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-12 10:59:59 +0000

    feat(front-end): modified these http methods based on chnaged types from kanbam.ts and also added new http methods.

--- src/utils/api/deletes.ts
+++ src/utils/api/deletes.ts
@@ -1,23 +1,46 @@
-import { ICard, IList } from "../../types/board.type";
 import { kanbamApi } from "./baseApi";
 
 // cards
 export const deleteCardById = async (id: string, token: string) => {
-  const { data } = await kanbamApi.delete<ICard>(`/cards/${id}`, {
+  await kanbamApi.delete(`/Cards/${id}/card`, {
     headers: {
       Authorization: `Bearer  ${token}`,
     },
   });
-  return data;
+};
+
+export const deleteCardByListId = async (listId: string, token: string) => {
+  await kanbamApi.delete(`/Cards/${listId}/list`, {
+    headers: {
+      Authorization: `Bearer  ${token}`,
+    },
+  });
 };
 
 // lists
 
 export const deleteListsById = async (id: string, token: string) => {
-  const { data } = await kanbamApi.delete<IList>(`/Lists/${id}`, {
+  await kanbamApi.delete(`/Lists/${id}`, {
     headers: {
       Authorization: `Bearer  ${token}`,
     },
   });
-  return data;
+};
+
+// board
+export const deleteBoardById = async (id: string, token: string) => {
+  await kanbamApi.delete(`/Boards/${id}`, {
+    headers: {
+      Authorization: `Bearer  ${token}`,
+    },
+  });
+};
+
+// workspace
+export const deleteWorkspaceById = async (id: string, token: string) => {
+  await kanbamApi.delete(`/Workspaces/${id}`, {
+    headers: {
+      Authorization: `Bearer  ${token}`,
+    },
+  });
 };
