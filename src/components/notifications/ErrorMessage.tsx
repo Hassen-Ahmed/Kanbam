44146b5a41acb05832841e528e911b326e31ed3e
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-15 21:04:16 +0000

    fix(front-end):modified by creating functionality to clear localStorage and logging out mechanisem to ErrorMessage component and also its styling

--- src/components/notifications/ErrorMessage.tsx
+++ src/components/notifications/ErrorMessage.tsx
@@ -1,14 +1,27 @@
 import { BiSolidError } from "react-icons/bi";
 import "./ErrorMessage.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function ErrorMessage() {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
   return (
     <div className="error-message-container">
-      <div className="logo">
-        <BiSolidError size={50} />
+      <div className="error-message">
+        <div className="logo">
+          <BiSolidError size={50} />
+        </div>
+        <h1>Sorry, something went wrong!</h1>
+        <h3>Try Logout, and Login again.</h3>
+        <div className="error-message__logout-btn">
+          <button onClick={handleNavigation}>Logout!</button>
+        </div>
       </div>
-      <h1>Sorry, something went wrong!</h1>
-      <h3>Try Logout, and Login again.</h3>
     </div>
   );
 }
