7f0efbb59dea69c5d1c819ac1b70affb32a80ad1
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-18 13:28:41 +0000

    feat(front-end): created new useDeletes custome hook to handle http delete requests and help to refactor the code. And also remove the previous method

---
+++ src/utils/api/useDeletes.tsx
@@ -0,0 +1,72 @@
+import { useCallback } from "react";
+import useKanbamApiClient from "./useKanbamApiClient";
+
+export default function useDeletes() {
+  const kanbamApi = useKanbamApiClient();
+
+  // workspace Member
+  const deleteWorkspaceMemberById = useCallback(
+    async (id: string) => {
+      await kanbamApi.delete(`/WorkspacesMembers/${id}`);
+    },
+    [kanbamApi]
+  );
+
+  // workspace
+  const deleteWorkspaceById = useCallback(
+    async (id: string) => {
+      await kanbamApi.delete(`/Workspaces/${id}`);
+    },
+    [kanbamApi]
+  );
+
+  // board Member
+  const deleteBoardMemberById = useCallback(
+    async (id: string) => {
+      await kanbamApi.delete(`/BoardsMembers/${id}`);
+    },
+    [kanbamApi]
+  );
+
+  // board
+  const deleteBoardById = useCallback(
+    async (id: string) => {
+      await kanbamApi.delete(`/Boards/${id}`);
+    },
+    [kanbamApi]
+  );
+
+  // lists
+  const deleteListsById = useCallback(
+    async (id: string) => {
+      await kanbamApi.delete(`/Lists/${id}`);
+    },
+    [kanbamApi]
+  );
+
+  // cards by listId
+  const deleteCardByListId = useCallback(
+    async (listId: string) => {
+      await kanbamApi.delete(`/Cards/${listId}/list`);
+    },
+    [kanbamApi]
+  );
+
+  // cards by id
+  const deleteCardById = useCallback(
+    async (id: string) => {
+      await kanbamApi.delete(`/Cards/${id}/card`);
+    },
+    [kanbamApi]
+  );
+
+  return {
+    deleteWorkspaceMemberById,
+    deleteWorkspaceById,
+    deleteBoardMemberById,
+    deleteBoardById,
+    deleteListsById,
+    deleteCardByListId,
+    deleteCardById,
+  };
+}
