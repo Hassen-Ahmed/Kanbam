9eb43f8e87c59fa54cd89df71ad138c1486f7105
Hassen-Ahmed <hassenbest23@gmail.com>
2024-11-25 22:20:37 +0000

    fix(front-end): modified by removing unnecessary token authentication handling

--- src/routes/auth/Auth.tsx
+++ src/routes/auth/Auth.tsx
@@ -1,24 +1,7 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./Auth.scss";
-import { useEffect, useState } from "react";
-import { isTokenAuthenticated } from "../../utils/jwtAuth";
-import Loading from "../../components/notifications/Loading";
 
 export default function Auth() {
-  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isTokenAuthenticated()) {
-      navigate("/");
-      setIsUserAuthenticated(true);
-    } else {
-      setIsUserAuthenticated(false);
-    }
-  }, [navigate]);
-
-  if (isUserAuthenticated) return <Loading />;
-
   return (
     <div className="auth">
       <Outlet />
