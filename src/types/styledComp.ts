c8f3e53e38436f72bfa7301a0c75d402a3adb079
Hassen-Ahmed <hassenbest23@gmail.com>
2024-12-23 19:13:07 +0000

    fix(front-end): modified styledComp util file based on themeList Context data and newly created Theme intereface

--- src/types/styledComp.ts
+++ src/types/styledComp.ts
@@ -1,26 +1,36 @@
-import { Theme } from "../utils/constantDatas/themes";
+import { ThemeGeneral } from "../utils/constantDatas/themes";
+import { Theme } from "./theme.type";
 
 export interface IBg {
-  $themename: keyof Theme;
-  $group: keyof Theme["light"]["bg"];
+  $themename: keyof ThemeGeneral;
+  $group: keyof ThemeGeneral["light"]["bg"];
+  $themeList: ThemeGeneral;
 }
 
 export interface IFont {
-  $themename: keyof Theme;
-  $group: keyof Theme["light"]["font"];
+  $themename: keyof ThemeGeneral;
+  $group: keyof ThemeGeneral["light"]["font"];
+  $themeList: ThemeGeneral;
 }
 
 export interface IBgAndFont {
-  $themename: keyof Theme;
-  $groupbg: keyof Theme["light"]["bg"];
-  $groupfont: keyof Theme["light"]["font"];
+  $themename: keyof ThemeGeneral;
+  $groupbg: keyof ThemeGeneral["light"]["bg"];
+  $groupfont: keyof ThemeGeneral["light"]["font"];
+  $themeList: ThemeGeneral;
 }
 
 export interface IBorder {
-  $themename: keyof Theme;
-  $group: keyof Theme["light"]["border"];
+  $themename: keyof ThemeGeneral;
+  $group: keyof ThemeGeneral["light"]["border"];
+  $themeList: ThemeGeneral;
 }
 
 export interface INewTheme {
-  $newtheme: "light" | "dark";
+  $newtheme: "light" | "dark" | "aiTheme";
+  $themeList: {
+    light: Theme;
+    dark: Theme;
+    aiTheme: Theme;
+  };
 }
