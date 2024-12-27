0dee862f890e827696af8b4eb846826cbe380d9f
Hassen-Ahmed <hassenbest23@gmail.com>
2024-12-27 01:10:51 +0000

    fix(front-end): modified members card styling by creating theres own className and styling

--- src/components/workspace/components/Members.tsx
+++ src/components/workspace/components/Members.tsx
@@ -165,7 +165,7 @@ export default function Members({ w_id, setShowMembers }: IMembers) {
 
           {dataWrMembers?.map((member) => {
             return (
-              <div key={member.id} className="item member">
+              <div key={member.id} className="item-member member">
                 <div className="item-name">
                   <h4>{member.userName}</h4>
                   <p className="desc">{member.email}</p>
