b07547881f70b7ea7b09b969f2f1c276f7bf0ec1
Hassen-Ahmed <hassenbest23@gmail.com>
2024-12-30 19:39:05 +0000

    feat(front-end): created new MenuBard which container all available buttons to edit or manipulate text

---
+++ src/components/card/modal/components/textEditor/MenuBar.tsx
@@ -0,0 +1,152 @@
+import { Editor } from "@tiptap/react";
+import { BsCode, BsTypeStrikethrough } from "react-icons/bs";
+import { GoListOrdered, GoListUnordered, GoQuote } from "react-icons/go";
+import { IoIosRedo, IoIosUndo } from "react-icons/io";
+import { VscCode } from "react-icons/vsc";
+
+interface IMenuBar {
+  editor: Editor;
+}
+
+const MenuBar = ({ editor }: IMenuBar) => {
+  if (!editor) return null;
+
+  return (
+    <div className="tiptap-btns">
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
+      >
+        B
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={editor.isActive("italic") ? "is-active" : ""}
+      >
+        <i>I</i>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        disabled={!editor.can().chain().focus().toggleStrike().run()}
+        className={editor.isActive("strike") ? "is-active" : ""}
+      >
+        <BsTypeStrikethrough />
+      </button>
+
+      {/* headings */}
+      <div className="custom-dropdown">
+        <button className="dropdown-toggle">Headings</button>
+        <div className="dropdown-menu">
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+            }
+          >
+            h1
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+            }
+          >
+            h2
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 3 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+            }
+          >
+            h3
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 4 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 4 }) ? "is-active" : ""
+            }
+          >
+            h4
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 5 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 5 }) ? "is-active" : ""
+            }
+          >
+            h5
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 6 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 6 }) ? "is-active" : ""
+            }
+          >
+            h6
+          </button>
+        </div>
+      </div>
+
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive("bulletList") ? "is-active" : ""}
+      >
+        <GoListUnordered />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive("orderedList") ? "is-active" : ""}
+      >
+        <GoListOrdered />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        disabled={!editor.can().chain().focus().toggleCode().run()}
+        className={editor.isActive("code") ? "is-active" : ""}
+      >
+        <BsCode />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive("codeBlock") ? "is-active" : ""}
+      >
+        <VscCode />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive("blockquote") ? "is-active" : ""}
+      >
+        <GoQuote />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+      >
+        <IoIosUndo />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+      >
+        <IoIosRedo />
+      </button>
+    </div>
+  );
+};
+
+export default MenuBar;
