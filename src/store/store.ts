978854c4fb96f91e759cdbe12e8183af9c8fd1fd
Hassen-Ahmed <hassenbest23@gmail.com>
2025-01-02 18:44:05 +0000

    feat(front-end): create store.ts file, which is define Redx store and export types based on store

---
+++ src/store/store.ts
@@ -0,0 +1,23 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { authReducer } from "../features/slices/authSlice";
+import { themeReducer } from "../features/slices/themeSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer.reducer,
+    theme: themeReducer.reducer,
+  },
+});
+
+// exporting types
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore["getState"]>;
+
+export type AppDispatch = AppStore["dispatch"];
+
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>;
