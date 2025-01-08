44eafdef3a011900abf5d362c4631afad301d26b
Hassen-Ahmed <hassenbest23@gmail.com>
2025-01-08 13:30:44 +0000

    fix(front-end): modified the data type and directly stored fetched workpsaces to data variable

--- src/hooks/useFetchAllWorkspace.tsx
+++ src/hooks/useFetchAllWorkspace.tsx
@@ -5,20 +5,16 @@ import useGets from "../utils/api/useGets";
 import axios from "axios";
 import { handlingAxioxError } from "../utils/errorHandling";
 
-interface IUseFetchAllWorkspace {
-  workspaces: IWorkspace[];
-}
-
 export default function useFetchAllWorkspace() {
   const { getAllWorkspace } = useGets();
-  const [data, setData] = useState<IUseFetchAllWorkspace | null>(null);
+  const [data, setData] = useState<IWorkspace[] | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<IError | null>(null);
 
   const fetchWorkspacesData = async () => {
     try {
       const response = await getAllWorkspace();
-      setData(response);
+      setData(response.workspaces);
     } catch (err) {
       if (axios.isAxiosError(err)) {
         const error = handlingAxioxError(err.response?.status) as IError;
