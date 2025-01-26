cade88e784920b05a2d11a70438f7cd19f86b66e
Hassen-Ahmed <hassenbest23@gmail.com>
2025-01-26 19:37:53 +0000

    feat(front-end): added new implementation like toastNotification, Accessibilities and also modified it styling

--- src/pages/auth/signup/SignUp.tsx
+++ src/pages/auth/signup/SignUp.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaArrowRotateLeft } from "react-icons/fa6";
-
 import { signupData } from "../../../utils/constantDatas/formData";
-import { IError } from "../../../types/status.type";
-
 import FormInput from "../formInput/FormInput";
-import "./SignUp.scss";
 import usePosts from "../../../utils/api/usePosts";
+import { logger } from "../../../utils/logger";
+import { useAppDispath } from "../../../features/hooks";
+import { toasterHandler } from "../../../utils/toaster";
+import { IError, StatusType } from "../../../types/kanbam";
+import "./SignUp.scss";
 
 export default function SignUp() {
   const { postAuthRegistarion } = usePosts();
-
-  const [isRegistered, setIsRegistered] = useState(false);
+  const dispatchRdx = useAppDispath();
+  const [requestStatus, setRequestStatus] = useState<StatusType>("idle");
   const [userDetails, setUserDetails] = useState({
     email: "",
     password: "",
@@ -22,28 +23,42 @@ export default function SignUp() {
 
   // end of hooks
 
-  const inputs = signupData(userDetails);
+  const formData = signupData(userDetails);
 
-  const handleOnChange = (ev: React.ChangeEvent<HTMLInputElement>) =>
+  const handleOnChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     setUserDetails((preValue) => {
       return { ...preValue, [ev.target.name]: ev.target.value };
     });
+    setRequestStatus("idle");
+  };
 
   const handleSignupForm = async (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
-    setIsRegistered(true);
+    setRequestStatus("loading");
 
     try {
       await postAuthRegistarion(userDetails);
+      setRequestStatus("idle");
+      toasterHandler({
+        dispathFun: dispatchRdx,
+        message: "Your registration was successful!",
+        duration: 5000,
+      });
       navigate("/auth/login");
     } catch (err) {
-      setIsRegistered(false);
+      setRequestStatus("failed");
+      toasterHandler({
+        dispathFun: dispatchRdx,
+        message: "Something went wrong!",
+        notificationType: "error",
+      });
+
       const error = err as IError;
-      console.log(error.message);
+      logger("error", error.message);
     }
   };
 
-  const loadingNotification = isRegistered && (
+  const loadingNotification = requestStatus === "loading" && (
     <span className="loading-notifiation">
       <FaArrowRotateLeft />
     </span>
@@ -52,30 +67,41 @@ export default function SignUp() {
   // JSX
 
   return (
-    <form onSubmit={(ev) => handleSignupForm(ev)} className="signup-form">
-      {inputs.map((input) => {
+    <form
+      onSubmit={handleSignupForm}
+      className="signup-form"
+      aria-live="polite"
+    >
+      {formData.map((input) => {
         return (
           <FormInput
             key={input.id}
             {...input}
             value={userDetails[input.name]}
+            aria-label={input.label}
             handleOnChange={handleOnChange}
           />
         );
       })}
 
       <div
-        className="signup-form_btn"
-        style={{ opacity: `${isRegistered ? "0.5" : "1"}` }}
+        className={`signup-form_btn ${
+          requestStatus !== "idle" ? "button-disabled" : ""
+        }`}
       >
-        <button disabled={isRegistered ? true : false}>
-          Sign up
+        <button
+          disabled={requestStatus !== "idle"}
+          aria-disabled={requestStatus !== "idle"}
+          aria-live="polite"
+          aria-label="Sign up button"
+        >
+          {requestStatus === "loading" ? "Sending..." : "Sign up"}
           {loadingNotification}
         </button>
       </div>
 
       <div className="signup__back-login">
-        <Link to={"/auth/login"}>
+        <Link to={"/auth/login"} aria-label="Login link">
           <p>
             Already have an account? <span> Login</span>
           </p>
