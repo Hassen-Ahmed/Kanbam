2692bfc55af146839f0f85aaa5ee003f56bd22da
Hassen-Ahmed <hassenbest23@gmail.com>
2025-01-30 11:10:05 +0000

    feat(front-end): implemented Google login with error handling and notifications. and added some styling

--- src/pages/auth/login/LogIn.tsx
+++ src/pages/auth/login/LogIn.tsx
@@ -9,12 +9,17 @@ import { useAppDispath } from "../../../features/hooks";
 import { setAsscessToken } from "../../../features/slices/authSlice";
 import { logger } from "../../../utils/logger";
 import { toasterHandler } from "../../../utils/toaster";
-import "./LogIn.scss";
 import { IError } from "../../../types/kanbam";
+import {
+  CredentialResponse,
+  GoogleLogin,
+  GoogleOAuthProvider,
+} from "@react-oauth/google";
+import "./LogIn.scss";
 
 const LogIn = () => {
   const dispatchRdx = useAppDispath();
-  const { postAuthLogin } = usePosts();
+  const { postAuthLogin, postAuthGoogleLogin } = usePosts();
   const [isAuthorizing, setIsAuthorizing] = useState(false);
   const [isWrongUser, setIsWrongUser] = useState(false);
   const [userDetails, setUserDetails] = useState({
@@ -25,7 +30,7 @@ const LogIn = () => {
   const navigate = useNavigate();
 
   // end of hooks
-
+  const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   const formData = loginData();
 
   const handleOnChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,7 +48,6 @@ const LogIn = () => {
       const data: { accessToken: string } = await postAuthLogin(userDetails);
 
       dispatchRdx(setAsscessToken(data.accessToken));
-
       navigate("/kanbam/w");
     } catch (err) {
       setIsAuthorizing(false);
@@ -61,64 +65,108 @@ const LogIn = () => {
     }
   };
 
+  const handleGoogleLogin = async (credentialResponse: CredentialResponse) => {
+    try {
+      toasterHandler({
+        dispathFun: dispatchRdx,
+        message: "Almost there, please wait...",
+        notificationType: "info",
+        duration: 3000,
+      });
+      const data = await postAuthGoogleLogin(credentialResponse.credential!);
+      toasterHandler({
+        dispathFun: dispatchRdx,
+        message: "Welcome to Kanbam!",
+        notificationType: "success",
+        duration: 3000,
+      });
+      dispatchRdx(setAsscessToken(data.accessToken));
+      navigate("/kanbam/w");
+    } catch (err) {
+      const error = err as IError;
+      logger("error", `Login failed: ${error.response?.data || error.message}`);
+    }
+  };
+
+  const handleGoogleLoginError = () => {
+    logger("error", `Login failed!`);
+  };
+
   // JSX
 
   return (
-    <form onSubmit={handleLoginForm} className="login-form" aria-live="polite">
-      {formData.map((input) => {
-        return (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={userDetails[input.name != "email" ? "password" : input.name]}
-            aria-label={input.label}
-            handleOnChange={handleOnChange}
-          />
-        );
-      })}
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <form
+        onSubmit={handleLoginForm}
+        className="login-form"
+        aria-live="polite"
+      >
+        {formData.map((input) => {
+          return (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={
+                userDetails[input.name != "email" ? "password" : input.name]
+              }
+              aria-label={input.label}
+              handleOnChange={handleOnChange}
+            />
+          );
+        })}
 
-      <div className="login-forgot-pw">
-        <Link to={"/auth/forgot-password"} aria-label="Forgot password link">
-          <span>Forgot Password?</span>
-        </Link>
-      </div>
+        <div className="login-forgot-pw">
+          <Link to={"/auth/forgot-password"} aria-label="Forgot password link">
+            <span>Forgot Password?</span>
+          </Link>
+        </div>
 
-      <div className={`login_btn ${isAuthorizing ? "button-disabled" : ""}`}>
-        <button
-          disabled={isAuthorizing}
-          aria-live="polite"
-          aria-label="Log in button"
-        >
-          {isAuthorizing ? "Sending..." : "Log in"}
-          {isAuthorizing && (
-            <span className="loading-notifiation">
-              <FaArrowRotateLeft />
-            </span>
-          )}
-        </button>
-      </div>
+        <div className={`login_btn ${isAuthorizing ? "button-disabled" : ""}`}>
+          <button
+            disabled={isAuthorizing}
+            aria-live="polite"
+            aria-label="Log in button"
+          >
+            {isAuthorizing ? "Sending..." : "Log in"}
+            {isAuthorizing && (
+              <span className="loading-notifiation">
+                <FaArrowRotateLeft />
+              </span>
+            )}
+          </button>
+        </div>
 
-      <div className="wrong-user">
-        {isWrongUser && <p>Wrong credential, please try again!</p>}
-      </div>
+        {GOOGLE_CLIENT_ID ? (
+          <div className="login-google">
+            <GoogleLogin
+              onSuccess={handleGoogleLogin}
+              onError={handleGoogleLoginError}
+              size="large"
+            />
+          </div>
+        ) : null}
+        <div className="wrong-user">
+          {isWrongUser && <p>Wrong credential, please try again!</p>}
+        </div>
 
-      <div className="tips">
-        <span>
-          <span className="label">EMAIL</span>: test@gmail.com
-        </span>
-        <span>
-          <span className="label">PASSWORD</span>: #Test1234
-        </span>
-      </div>
+        <div className="tips">
+          <span>
+            <span className="label">EMAIL</span>: test@gmail.com
+          </span>
+          <span>
+            <span className="label">PASSWORD</span>: #Test1234
+          </span>
+        </div>
 
-      <div className="login__create-account">
-        <Link to={"/auth/signup"} aria-label="Create new account link">
-          <p>
-            No account? <span>Create one</span>
-          </p>
-        </Link>
-      </div>
-    </form>
+        <div className="login__create-account">
+          <Link to={"/auth/signup"} aria-label="Create new account link">
+            <p>
+              No account? <span>Create one</span>
+            </p>
+          </Link>
+        </div>
+      </form>
+    </GoogleOAuthProvider>
   );
 };
 
